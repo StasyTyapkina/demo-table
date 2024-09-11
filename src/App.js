0a8fed04c9f8f81be6e0612d5a9fb1a301d488ad
Stasya <stasya.tyapkina@gmail.com>
2024-09-11 12:52:57 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Footer } from "./components/Footer";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { UserInfoPage } from "./pages/UserInfoPage";
 import { UserDetailPage } from "./pages/UserDetailPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
@@ -9,7 +9,8 @@ export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<UserInfoPage />} />
+        <Route exact path={"/"} element={<Navigate to={"/main"} />} />
+        <Route path="/main" element={<UserInfoPage />} />
         <Route path="/user/:id" element={<UserDetailPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
